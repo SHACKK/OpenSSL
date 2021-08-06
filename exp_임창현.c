78464c4de763a730abbec47921d00b165e96c4d5
SHACKK <jfhg456@kakao.com>
2021-08-07 04:51:34 +0900

    R2L 알고리즘

--- exp_임창현.c
+++ exp_임창현.c
@@ -8,6 +8,35 @@ void printBN(char *msg, BIGNUM * a)
         OPENSSL_free(number_str);
 }
 
+BIGNUM *ExpMod(BIGNUM *res, BIGNUM *a, BIGNUM *e, BIGNUM *m) {
+        BIGNUM *A = BN_new();
+        BN_dec2bn(&A, "1");
+        BIGNUM *B = BN_new();
+        BN_copy(B, a);
+        // BIGNUM *q = BN_new();
+        // BN_dec2bn(q, "1");
+        BIGNUM *two = BN_new();
+        BN_dec2bn(&two, "2");
+        BIGNUM *r = BN_new();
+        BN_CTX *ctx = BN_CTX_new();
+
+        while (!BN_is_zero(e))
+        {
+                BN_div(e,r,e, two, ctx);
+                if (BN_is_one(r)) {
+                        BN_mul(A, A, B, ctx);
+                        BN_mul(B, B, B, ctx);
+                } else {
+                        BN_mul(B, B, B, ctx);
+                }
+                BN_mod(A, A, m, ctx);
+                BN_mod(B, B, m, ctx);
+        }
+
+        BN_copy(res, A);
+        return res;
+}
+ 
 int main (int argc, char *argv[])
 {
         BIGNUM *a = BN_new();
@@ -27,7 +56,7 @@ int main (int argc, char *argv[])
         printBN("e = ", e);
         printBN("m = ", m);
 
-        ExpMod(res,a,e,m);
+        res = ExpMod(res,a,e,m);
 
         printBN("a**e mod m = ", res);
 
